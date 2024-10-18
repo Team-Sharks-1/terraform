f7527a8c6a009cc037f9f22d6ff6f604d877b551
armanlamba <armanlamba28@gmail.com>
2024-10-18 01:11:32 -0400

    modified:   terraform/main.tf 	modified:   terraform/outputs.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -109,7 +109,59 @@ resource "azurerm_linux_virtual_machine" "vm" {
     version   = "latest"
   }
 
-  custom_data = base64encode(file("scripts/init.sh"))
+  custom_data = base64encode(file("C:/Users/arman/OneDrive/Desktop/terraform/scripts/init.sh"))
+
+}
+
+#Ansible vm
+resource "azurerm_network_interface" "ansible_nic" {
+  name                = "${var.prefix}-ansible-nic" # NIC for Ansible VM
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.ansible_publicip.id 
+  }
+}
+
+resource "azurerm_public_ip" "ansible_publicip" {
+  name                = "${var.prefix}-ansible-publicip" # Public IP for Ansible VM
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_linux_virtual_machine" "ansible_vm" {
+  name                = "${var.prefix}-ansible-vm" # New Ansible VM
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1s" # Using variable for VM size
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.ansible_nic.id,
+  ]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  custom_data = base64encode(file("C:/Users/arman/OneDrive/Desktop/terraform/scripts/init.sh"))
 }
 
 # resource "azurerm_mysql_server" "mysql" {
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -8,13 +8,23 @@ output "public_ip_address" {
   value = azurerm_public_ip.publicip.ip_address
 }
 
+# Output for Ansible host VM's public IP address
+output "ansible_vm_public_ip" {
+  value = azurerm_public_ip.ansible_publicip.ip_address
+}
+
+# Output for Ansible host VM's name
+output "ansible_vm_name" {
+  value = azurerm_linux_virtual_machine.ansible_vm.name # Output for Ansible VM's name
+}
+
 # output "mysql_server_name" {
 #   value = azurerm_mysql_server.mysql.name
 # }
 
-output "mysql_server_fqdn" {
-  value = azurerm_mysql_server.mysql.fqdn
-}
+# output "mysql_server_fqdn" {
+#   value = azurerm_mysql_server.mysql.fqdn
+# }
 
 output "storage_account_name" {
   value = azurerm_storage_account.storage.name
